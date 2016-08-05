52cd189af31a658a671f39b730e7c2ddd18d59e1
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-05 00:00:52 -0400

    Add services.root pkg

---
+++ clara_std/services/root/HistogramWriterService.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+from clara.engine.Engine import Engine
+
+
+class HistogramWriterService(Engine):
+
+    def get_input_data_types(self):
+        pass
+
+    def destroy(self):
+        pass
+
+    def get_output_data_types(self):
+        pass
+
+    def execute_group(self, inputs):
+        pass
+
+    def get_description(self):
+        pass
+
+    def execute(self, engine_data):
+        pass
+
+    def configure(self, engine_data):
+        pass
+
+    def get_version(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def get_states(self):
+        pass
+
+    def get_author(self):
+        pass
+
+    def __init__(self):
+        pass
